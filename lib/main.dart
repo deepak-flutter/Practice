a603f77cecc0c64b564b7d9eb64332d3915c598b
Deepak <deepak.unibit@gmail.com>
2024-06-27 10:38:07 +0530

    dfsg

--- lib/main.dart
+++ lib/main.dart
@@ -49,9 +49,7 @@ Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
 class MyHttpOverrides extends HttpOverrides {
   @override
   HttpClient createHttpClient(SecurityContext? context) {
-    return super.createHttpClient(context)
-      ..badCertificateCallback =
-          (X509Certificate cert, String host, int port) => true;
+    return super.createHttpClient(context)..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
   }
 }
 
